0c76e4a7608e9227d536a8b87e886b0a0ba629df
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-09-05 23:53:18 +0530

    Add files via upload

--- 2nd.c
+++ 2nd.c
@@ -1,1 +1,14 @@
-//Write a C program to print the value of addition of two numbers:
+// Write a C program to print the value of addition of two numbers:
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int A, B, C;
+    printf("Enter the value of A:");
+    scanf("%d", &A);
+    printf("Enter the value of B:");
+    scanf("%d", &B);
+    C = A + B;
+    printf("The value of addition:%d", C);
+    return 0;
+}
