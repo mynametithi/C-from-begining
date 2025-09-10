7f899d72c0c42a59e95233dbd0bf2d6bda08df03
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-09-10 19:51:05 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,17 @@
+//pattern 
+#include<stdio.h>
+ int main(){
+    int n,i,j,k; 
+    printf("Enter a number of n:     ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=(n-i);j++){
+            printf(" ");
+        }
+        for(k=1;k<=i;k++){
+            printf("%d",k);
+        }
+        printf("\n");
+    }
+    return 0;
+ }
