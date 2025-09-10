ba5d44343ad82f9a8168c4c0f64b3174b0bbad2a
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-09-10 19:50:43 +0530

    Add files via upload

---
+++ factorialOfN.cpp
@@ -0,0 +1,13 @@
+//Factorial of N number :
+//using function:
+#include<stdio.h>
+int main(){
+    int i,n,fact=1;
+    printf("Enter a number of n:   ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        fact*=i;
+    }
+    printf("The value is:%d",fact);
+    return 0;
+}
